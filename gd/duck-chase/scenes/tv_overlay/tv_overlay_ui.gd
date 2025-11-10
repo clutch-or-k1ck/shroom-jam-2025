9cf8a13f14f89af267683de5249b84ce167414fa
clutch-or-kick <spoklonskyi@gmail.com>
2025-11-10 20:10:17 +0100

    minor changes to the old overlay, new tv overlay, and duck positioning

--- gd/duck-chase/scenes/tv_overlay/tv_overlay_ui.gd
+++ gd/duck-chase/scenes/tv_overlay/tv_overlay_ui.gd
@@ -4,7 +4,7 @@ extends NinePatchRect
 var duck_chase_scene := preload("res://scenes/chase_scene/chase_scene.tscn")
 @onready var subviewport := $SubViewport
 
-		
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# instantiate the chase scene into the subviewport...
@@ -13,6 +13,35 @@ func _ready() -> void:
 	pass
 
 
+func draw_vertical_line(at: int) -> void:
+	draw_line(Vector2(at, -10000), Vector2(at, 10000), Color(1., 0., 0.))
+
+
+func draw_horizontal_line(at: int) -> void:
+	draw_line(Vector2(-10000, at), Vector2(10000, at), Color(1., 0., 0.))
+
+
+func _draw() -> void:
+	# specify the thickness of the tvset panels
+	const top_width := 130
+	const bottom_width := 120
+	const left_width := 150
+	const right_width := 550
+	
+	# how much the GUI is downscaled due to stretching
+	var stretch_factor := get_window().get_stretch_transform().get_scale()[0]
+	
+	var left_v := left_width * stretch_factor
+	var right_v := get_viewport_rect().size[0] - right_width * stretch_factor
+	var top_h := top_width * stretch_factor
+	var bottom_h := get_viewport_rect().size[1] - bottom_width * stretch_factor
+	
+	draw_vertical_line(left_v)
+	draw_vertical_line(right_v)
+	draw_horizontal_line(top_h)
+	draw_horizontal_line(bottom_h)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(get_window().get_stretch_transform().get_scale())
